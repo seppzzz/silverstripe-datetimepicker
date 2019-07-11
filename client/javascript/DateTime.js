b16465a3f4c5b601bca32119ba76acbd22a057f8
agencecaza <51217595+agencecaza@users.noreply.github.com>
2019-07-11 14:43:42 -0400

    Update DateTime.js

--- client/javascript/DateTime.js
+++ client/javascript/DateTime.js
@@ -2,6 +2,38 @@ jQuery.noConflict();
 
 (function($) {
 
+    $('input.time').entwine({
+
+        onfocusin: function() {
+
+            $('input.time').datetimepicker({
+                format: 'H:i',
+                datepicker: false,
+                timepicker: true,
+            });
+
+
+        }
+
+    });
+
+    $('input.date').entwine({
+
+        onfocusin: function() {
+
+            $('input.date').datetimepicker({
+                format: 'Y-m-d',
+                datepicker: true,
+                timepicker: false,
+            });
+
+
+        }
+
+    });
+
+
+
     $('input.datetime').entwine({
 
         onfocusin: function() {
@@ -10,7 +42,7 @@ jQuery.noConflict();
                 format: 'Y-m-d H:i:s',
                 timepicker: true,
             });
-            
+
 
         }
 
