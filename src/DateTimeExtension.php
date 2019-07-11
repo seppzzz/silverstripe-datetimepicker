df708e466fe93fe5639eab27f8810da848fdab2e
Stéphane Guevremont <stephane.guevremont@gmail.com>
2019-07-10 21:42:13 -0400

    Update DateTimeExtension.php

--- src/DateTimeExtension.php
+++ src/DateTimeExtension.php
@@ -1,22 +1,23 @@
 <?php
 
+namespace Intwebg\Fields;
+
 use SilverStripe\View\Requirements;
 use SilverStripe\Core\Extension;
 use SilverStripe\Admin\LeftAndMain;
 use SilverStripe\i18n\i18n;
 
-namespace Intwebg\Fields {
 
-	class DateTimeFieldExtension extends Extension {
+class DateTimeFieldExtension extends Extension {
 
-		public function init() {
+	public function init() {
 
-			Requirements::css('intwebg/datetimepicker: thirdparty/datetimepicker/build/jquery.datetimepicker.min.css');
-			Requirements::javascript('intwebg/datetimepicker: thirdparty/datetimepicker/build/jquery.datetimepicker.full.min.js');
-			Requirements::javascript('intwebg/datetimepicker: client/javascript/DateTime.js');
-			Requirements::customScript("
-				jQuery.datetimepicker.setLocale('".substr(i18n::get_locale(), 0, 2)."');
-			");
-		}
+		Requirements::css('intwebg/datetimepicker: thirdparty/datetimepicker/build/jquery.datetimepicker.min.css');
+		Requirements::javascript('intwebg/datetimepicker: thirdparty/datetimepicker/build/jquery.datetimepicker.full.min.js');
+		Requirements::javascript('intwebg/datetimepicker: client/javascript/DateTime.js');
+		Requirements::customScript("
+			jQuery.datetimepicker.setLocale('".substr(i18n::get_locale(), 0, 2)."');
+		");
 	}
 }
+
